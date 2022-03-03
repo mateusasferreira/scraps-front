61d32fe83dac8599ffad01e21ee9de1780dfc1eb
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2022-03-03 17:58:32 -0300

    feat: styled-components config

---
+++ styles/theme.ts
@@ -0,0 +1,25 @@
+const theme = {
+  colors: {
+    bgWhite: '#f8f5f3',
+    highlightWhite: '#e8ebed',
+    black: '#242424',
+    highlightColor: '#5268ff',
+    secondaryColor: '#409fff',
+    thirdColor: '#aab5ff' 
+  },
+  fontsFamily: {
+    header: '\'Merriweather\', serif',
+    standard: '\'Poppins\', sans-serif'
+  },
+  fontsWeight: {
+    regular: 400,
+    bold: 600,
+    xbold: 700
+  },
+  shadows: {
+    deep: 'rgba(0, 13, 96, 0.35) 0px 5px 15px;',
+    smooth: 'rgba(36, 36, 36, 0.2) 0px 8px 24px;'
+  }
+}
+
+export default theme
